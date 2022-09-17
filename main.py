2a3e246bf3bac30a9c3b78497884cb9e077646c5
Sloan-James <32432826+Sloan-James@users.noreply.github.com>
2022-09-17 13:18:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class Bids:
     self.itemBidders = []
 
 global guildID
-guildID = os.environ["GUILD_ID"]
-channelID = os.environ["CHANNEL_ID"]
+guildID = int(os.environ["GUILD_ID"]) 
+channelID = int(os.environ["CHANNEL_ID"]) 
 
 class aclient(discord.Client):
   def __init__(self):
